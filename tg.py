7ba230cb6973d8bf6507357740359d13f18c6dfe
NoBadVibes79 <krazerog@outlook.com>
2024-02-26 15:58:30 +0300

    clear code 1

--- tg.py
+++ tg.py
@@ -1,9 +1,6 @@
-from telebot import types
-from db.sqlite import SQLite
 from worker import Worker
 import re
 import telebot
-import requests
 import time
 
 bot = telebot.TeleBot('7014412419:AAFiQ0toKgiXt4zqPlGvWpR4ojwJLfjrPgQ')
@@ -61,6 +58,7 @@ def check_accs(message):
 Чтобы я начал пришли мне данные в следующем формате - *LabelГруппыНачало,LabelГруппыКонец*
 Пример - *01.01 100.1,02.01.30*''', parse_mode="Markdown")
 
+
 @bot.message_handler(commands=['pages'])
 def create_pages(message):
     bot.send_message(message.from_user.id, '''Создание фанпейджей
@@ -74,11 +72,6 @@ def get_user_text(message):
         global proxy_id, worker
         PATTERN_CHECK = r"\d,\d"
         PATTERN_CREATE = r"^pg"
-        USERNAME = message.from_user.username
-        ID_USER = message.from_user.id
-        
-        TABLE = USERNAME
-        # db.create_table_if_not(TABLE, MEMORY)
         
         if len(message.text) == 5 and message.text.isdigit():
             proxy_id = message.text
@@ -111,65 +104,6 @@ def get_user_text(message):
         
     except:
         pass
-            
-        
-#         # global table
-#         # global ID_PERS
-#         # global ID_MESSAGE_EDIT
-        
-#         # db=SQLite("db\\dialogs.db")
-#         gpt.prompts = message.text
-        
-#         db.take_all(TABLE)
-        
-#         system_prompt = system_prompt + "To answer the next question these data may be relevant: "
-        
-#         # for el in db.rows:
-#         #     if len(system_prompt) >=4500:
-#         #         system_prompt = system_prompt[:4000]
-#         #         break
-                
-#         #     system_prompt += el[0] + ' '
-        
-#         # messages = [{"role": "user", "content": {gpt.prompts}}]
-#         # context = db.take_context()
-#         # messages.extend(context)
-        
-#         # system_prompt = 'To format text, use markdownv2 for telegram. This is the previous history of your dialogue with this person, take it into consideration: ' 
-#         response = gpt.talk_valid_markdown(prompts=gpt.prompts)
-        
-#         bot.send_message(message.from_user.id, response, parse_mode="Markdown")
-        
-        
-#         # if MEMORY == True:
-#         #     try:
-#         #         db.insert(TABLE, gpt.prompts, 'user')
-#         #         db.insert(TABLE, response, 'assistant')
-#         #     except Exception as e:
-#         #         print(e)
-#         #         bot.send_message(722895694, e)
-                
-#         #     db.take_all(TABLE)
-            
-            
-#         #     #     print(type(el))
-#         #     #     bot.send_message(722895694, el)
-#         #     #     time.sleep(2)
-                
-                
-            
-#         #     print(db.rows)
-#         #     # print(type(db.rows))
-        
-        
-            
-    # except Exception as e:
-    #     print(e)
-    #     with open('log_tg.txt', 'a', encoding="utf-8") as file:
-    #         file.write(f'\n{str(e)}')
-    #     bot.send_message(message.from_user.id,  '''Простите, сейчас мой создатель меня дорабатывает, попробуйте позже 
-    #     Если проблема *долго* не решается напишите нашему создателю *@Sad_Manners*
-    #                      ''', parse_mode="Markdown")
 
 
 if __name__ == '__main__':
